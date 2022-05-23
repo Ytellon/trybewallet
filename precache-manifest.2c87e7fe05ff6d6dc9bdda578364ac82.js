842f120f448e7633458dd561b7d69f9536fbe018
Ytellon Macedo <ytellonmacedo@gmail.com>
2022-05-23 11:01:33 -0300

    Updates

---
+++ precache-manifest.2c87e7fe05ff6d6dc9bdda578364ac82.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32ad43b601e3a153bd5a1a2627bd2777",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "b5bb27b16c4af4174ec2",
+    "url": "/trybewallet/static/css/main.d910602a.chunk.css"
+  },
+  {
+    "revision": "0ef0becbb1b6b7a14a44",
+    "url": "/trybewallet/static/js/2.6aacd6fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.6aacd6fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5bb27b16c4af4174ec2",
+    "url": "/trybewallet/static/js/main.63877f60.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/trybewallet/static/js/main.63877f60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  },
+  {
+    "revision": "ab282aff33080fd01b44997dfc2db558",
+    "url": "/trybewallet/static/media/womanImg.ab282aff.svg"
+  }
+]);
